30a7f6fec3503583dfa83bd4849266ba3a583b5c
Ben Slater <bslater@nmu.edu>
2016-04-14 12:26:49 -0400

    added login functionality to client.js

--- pub/client.js
+++ pub/client.js
@@ -1,3 +1,33 @@
-function hideLogin{
+socket = io();
+function hideLogin(obj){
 	//on a successful login or account creation, hide initial login screen
+	socket.emit("login", obj);
+	socket.on("loginValidation",function (msg){
+		return msg;
+	}
 }
+
+function login(){
+	var obj = {
+		obj.username = $("#username").val();
+		obj.password = $("#password").val();
+		obj.message = $("#username").val();
+	}
+	$("#loginButton").click(function()
+			obj.message = "login";
+		if(hideLogin(obj)){
+			$("#loginScreen").hide();
+		});
+	$("#createButton").click(function(){
+		obj.message = "create";
+		if(hideLogin(obj)){
+			$("#loginScreen").hide();
+		}
+	});
+}
+
+function startUp(){
+
+}
+
+$(startUp);
