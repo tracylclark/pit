cb65c8c1d4e800bb4bc5b570323de56cc08c0eb4
Ben Slater <bslater@nmu.edu>
2016-04-17 17:05:39 -0400

    added click handlers on client side for cards

--- pub/client.js
+++ pub/client.js
@@ -1,4 +1,8 @@
 socket = io();
+var user;
+var hand = [];
+var trades;
+trades.cards = [];
 function escapeHTML(theString) {
 	return theString
 		.replace(/&/g, "&amp;")
@@ -11,7 +15,7 @@ function escapeHTML(theString) {
 
 function hideLogin(obj){
 	//on a successful login or account creation, hide initial login screen
-	
+
 }
 
 function login(){
@@ -29,8 +33,9 @@ function startUp(){
 		//increment game scores, create a round won by message, etc
 		updateGUI();
 	});
-	
+
 	$("#loginButton").click(function(){
+		user = $("#username").val();
 		var obj = {
 			username: $("#username").val(),
 			password: $("#password").val()
@@ -41,6 +46,7 @@ function startUp(){
 		socket.emit("login", obj);
 		}});
 	$("#createButton").click(function(){
+		user = $("#username").val();
 		var obj = {
 			username: $("#username").val(),
 			password: $("#password").val()
@@ -59,8 +65,111 @@ function startUp(){
 	});
 	//need to create an array of card objects that they want to trade as a global, then the click button will
 	//send that to the server
+	//clickHandlier on cards, will put selected cards objects onto trades.cards []
+	$("#cell0").click(function{
+		if(find(trades.cards, 0)){
+			highlightCard("#cell0");
+			trades.cards.push(0);
+		}
+		else{
+			unhighlightCard("#cell0");
+			trades.cards.splice(0,1);
+		}
+	});
+	$("#cell1").click(function{
+		if(find(trades.cards, 1)){
+			highlightCard("#cell1");
+			trades.cards.push(1);
+		}
+		else{
+			unhighlightCard("#cell1");
+			trades.cards.splice(1,1);
+		}
+	});
+	$("#cell2").click(function{
+		if(find(trades.cards, 2)){
+			highlightCard("#cell2");
+			trades.cards.push(2);
+		}
+		else{
+			unhighlightCard("#cell2");
+			trades.cards.splice(2,1);
+		}
+	});
+	$("#cell3").click(function{
+		if(find(trades.cards, 3)){
+			highlightCard("#cell3");
+			trades.cards.push(3);
+		}
+		else{
+			unhighlightCard("#cell3");
+			trades.cards.splice(3,1);
+		}
+	});
+	$("#cell4").click(function{
+		if(find(trades.cards, 4)){
+			highlightCard("#cell4");
+			trades.cards.push(4);
+		}
+		else{
+			unhighlightCard("#cell4");
+			trades.cards.splice(4,1);
+		}
+	});
+	$("#cell5").click(function{
+		if(find(trades.cards, 5)){
+			highlightCard("#cell5");
+			trades.cards.push(5);
+		}
+		else{
+			unhighlightCard("#cell5");
+			trades.cards.splice(5,1);
+		}
+	});
+	$("#cell6").click(function{
+		if(find(trades.cards, 6)){
+			highlightCard("#cell6");
+			trades.cards.push(6);
+		}
+		else{
+			unhighlightCard("#cell6");
+			trades.cards.splice(6,1);
+		}
+	});
+	$("#cell7").click(function{
+		if(find(trades.cards, 7)){
+			highlightCard("#cell7");
+			trades.cards.push(7);
+		}
+		else{
+			unhighlightCard("#cell7");
+			trades.cards.splice(7,1);
+		}
+	});
+	$("#cell7").click(function{
+		if(find(trades.cards, 7)){
+			highlightCard("#cell7");
+			trades.cards.push(7);
+		}
+		else{
+			unhighlightCard("#cell7");
+			trades.cards.splice(7,1);
+		}
+	});
+	$("#cell8").click(function{
+		if(find(trades.cards, 8)){
+			highlightCard("#cell8");
+			trades.cards.push(8);
+		}
+		else{
+			unhighlightCard("#cell8");
+			trades.cards.splice(8,1);
+		}
+	});
 	$("#trade").click(function()){
+		trades.user = user;
 		socket.emit("trade", trades);
+		trades.cards = [];
 	}
 	$("#corner".click(function())){
 		socket.emit("corner");
@@ -72,6 +181,24 @@ function startUp(){
 		}
 	}
 	//when we build the scoreboard then we just id each row appended by the users name
+}
+
+function highlightCard(cell){
+	$(cell).css("background-color", "rgba(255,180,0,.5)");
+}
+}
+
+function unhighlightCard(cell){
+	$(cell).css("background-color", "");
+}
+
+function find(array, val){
+	for(var i = 0; i < array.length(); i++){
+		if(i == val){
+			return true;
+		}
+	}
+	return false;
 }
 
 $(startUp());
