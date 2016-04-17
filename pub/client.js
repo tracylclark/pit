422fe3d192ed31c16c504bc5aa80b8c5b4621984
Tracy Clark <Tracy Clark>
2016-04-17 12:30:17 -0400

    did some GUI updates and put in code for sanitizing input, added some server messages/responses

--- pub/client.js
+++ pub/client.js
@@ -1,20 +1,42 @@
 socket = io();
+function escapeHTML(theString) {
+	return theString
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/\//g, "&#47;")  // \/  represents the / here.
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#39;");
+}
+
 function hideLogin(obj){
 	//on a successful login or account creation, hide initial login screen
+	
+}
+
+function login(){
+
+}
+
+
+function startUp(){
 	socket.emit("login", obj);
 	socket.on("loginValidation",function (msg){
 		return msg;
 	});
-}
+	socket.on("gameWin", function(msg){
+		//update the GUI, display a msg with who won, etc
+		updateGUI();
 
-function login(){
-	var obj = {
-		username: $("#username").val(),
-		password: $("#password").val()
-	};
+	});
+	socket.on("roundWin", function(msg){
+		//increment game scores, create a round won by message, etc
+		updateGUI();
+	});
+	
 	$("#loginButton").click(function(){
 			obj.message = "login";
-		if(hideLogin(obj)){
+		if(hideLogin(obj)){ //this should only happen on a valid login
 			$("#loginScreen").hide();
 		}});
 	$("#createButton").click(function(){
@@ -23,11 +45,13 @@ function login(){
 			$("#loginScreen").hide();
 		}
 	});
-}
-
-
-function startUp(){
-	login();
+	var obj = {
+		username: $("#username").val(),
+		password: $("#password").val()
+	};
+	obj.username = escapeHTML(obj.username); //sanitize the input username before sending it to the server
+	obj. password = escapeHTML(obj.password); //sanitize the input password before sending it to the server
+	// login();
 }
 
 $(startUp());
