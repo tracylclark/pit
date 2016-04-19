44c4baef1a80ca09f07b66f18ceb5a40840c80ca
Tracy Clark <Tracy Clark>
2016-04-19 11:01:54 -0400

    Changed the html to reflect more updated gameplay. Changed some of the database functions in server to make sure that a login can't take place for someone who has already logged in (players must be unique). Also went through and made some chages to the socket.on messages, they should cover more of the bases now -- such as login now creating player and spectator objects and pushing them on a successful login.

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ var server = http.Server(app);
 var socketIo = require("socket.io");
 var io = socketIo(server);
 app.use(express.static("pub"));
+
 var db;
-var numberOfPlayers;
+// var numberOfPlayers;
 var gameMode = 0; // gameMode 0 is waiting to start, 1 is in game, 2 is game over
 var ready = 0; //increment on ready message, check for gameReady status
 var players = [];
@@ -115,7 +116,7 @@ function userQuery(db, userName, callback) {
 	});
 }
 
-function createUser(db, userName, passWord, callback){ //if null and msg==create createUser()
+function createUser(db, userName, passWord, callback){ //we don't have to check for uniqueness in UN here 
 	var collection = db.collection("users");
 	collection.insertOne({username : userName, password: passWord, wins: 0, losses: 0}, function(err, result){
 		if (err!=null) callback("error");
@@ -130,7 +131,12 @@ function loginUser(db, userName, passWord, callback){ //could make this much lea
 			console.log("Error on login trying to find " + err);
 			callback("error");
 		}
-		else callback(docs); //returns null if no un / pw combination that matches
+		else{
+			for (var i = 0; i<players.length(); i++){
+				if (userName==players[i].name) callback(null); //can't have someone try to log into the same account twice
+			} //doublecheck this works
+			callback(docs); //returns null if no un / pw combination that matches	
+		} 
 	});
 }
 
@@ -206,28 +212,20 @@ function updateGameState(){
 io.on("connect", function(socket) { //was "connection"
 	console.log("Somebody connected to our socket.io server");
 
-	socket.on("disconnect", function() { //need to change game mode on disconnect if a player
+	socket.on("disconnect", function() { 
 		console.log("Somebody left.");
-
-		var indexOfUser = allSockets.indexOf(socket);
-		allSockets.splice(indexOfUser, 1); //index to remove at, how many elements to remove.
-		allUsernames.splice(indexOfUser, 1); //index to remove at, how many elements to remove.
-		if (indexOfUser<8){
+		var indexOfUser = spectators.map(function(e) { return e.socket; }).indexOf(socket);
+		if (indexOfUser!=-1){
+			allSockets.splice(indexOfUser, 1); //index to remove at, how many elements to remove.
+			allUsernames.splice(indexOfUser, 1); //index to remove at, how many elements to remove.
+		}
+		else{ 
+			indexOfUser = players.map(function(e) { return e.socket; }).indexOf(socket);
 			players.splice(indexOfUser, 1);
 			sockets.splice(indexOfUser, 1);
-			gameMode = 2; //game over bc we don't have enough players
-			//if someone leaves and game has started will have to reset the entire game
+			if (gameMode==1) gameMode = 2; //game over if a player leaves during play
 		}
-		//need to send a gameMode update in case person leaving was a player
-		// io.emit("updateUserList", allUsernames);
 		updateGameState();
-	});
-	socket.on("ready", function(socket){
-		ready+=1;
-		if(readyToPlay()){
-			dealDeck();
-			updateGameState();
-		}
 	});
 
 	socket.on("login", function(obj) { 
@@ -241,7 +239,11 @@ io.on("connect", function(socket) { //was "connection"
 			if (result != null && msg=="login"){
 				//use the db to check password for login
 				loginUser(db, un, pw, socket, function(result){
-					if(result!=null) io.emit("loginValidation", true);
+					if(result!=null && result!="error"){
+						if (players.length()<=8) players.push({name: un, socket: socket, wins: result[0].wins, losses: result[0].losses, score: 0});
+						else spectators.push({name: un, socket: socket}); //don't need wins, losses, or score for spectators
+						io.emit("loginValidation", true);
+					} 
 					else io.emit("loginValidation, false");
 				});
 				
@@ -249,7 +251,11 @@ io.on("connect", function(socket) { //was "connection"
 			else if (result == null && msg=="create"){
 				//use the db to create a user
 				createUser(db, un, pw, socket, function(result){
-					if(result!=null) io.emit("loginValidation", true); 
+					if(result!=null && result!="error"){ 
+						if (players.length()<=8) players.push({name: un, socket: socket, wins: 0, losses: 0, score: 0});
+						else spectators.push({name: un, socket: socket});
+						io.emit("loginValidation", true); 
+					} 
 					else io.emit("loginValidation", false); //create didn't succeed
 				});
 			} 
@@ -259,12 +265,13 @@ io.on("connect", function(socket) { //was "connection"
 		});
 
 		updateGameState();
-		
-		//add to player list (unless too large, then spectator)
-		
-
-		// io.emit("loginValidation", msg); 
-		//pass the user objects on game update (when events occur)
+	});
+	socket.on("ready", function(socket){
+		ready+=1;
+		if(readyToPlay()){
+			dealDeck();
+			updateGameState();
+		}
 	});
 
 	
@@ -280,39 +287,39 @@ io.on("connect", function(socket) { //was "connection"
 		}
 		updateGameState();
 	});
-	socket.on("corner", function(corner, player, gameMode){
-		var round = checkForRoundWin();
+	socket.on("corner", function(corner, player, gameMode){ //what is happening here, we don't get anything from client side on click
 		var game = false;
-		if (round){
-			players[player].score += hands[player][0].points;
-			game = checkForGameWin();
-		}
-
-		//round win && game win
-		if (round && game){
-			//update db : everyone who isn't the winner gains a loss, winner gains a win
-			updateScores(db, function(result){
-				//not sure if this will work, i have to update ALL the scores but don't have a filter for the db, so using a for loop currently
-				if (result==null) console.log(); //log whose data wasn't updated (manually change if error??)
-			});
-			io.emit("gameWin", msg); //msg is who won
-		}
+		var round = checkForRoundWin();
+		var playerIndex = players.map(function(e) { return e.socket; }).indexOf(socket); 
+		if (playerIndex == -1) io.emit("noWin"); //whoever clicked wasn't a player
 		else if (round){
-			//msg is round winner, increment score, change gameMode to redeal WHICH GAME MODE DOES THIS!!!
-			dealDeck();
-			lastRoundWinner = player; //should be a string for player name
-			io.emit("roundWin", player);
+			players[playerIndex].score += hands[playerIndex][0].points; //increment their score based on their first card
+			game = checkForGameWin(); //check to see if the game is over
+			if (game){
+				updateScores(db, function(result){
+				//not sure if this will work, i have to update ALL the scores but don't have a filter for the db, so using a for loop currently
+				if (result=="error") console.log("Error on updating player scores in the database."); 
+				});
+				gameMode = 0;
+				readyToPlay = 0;
+				io.emit("gameWin", players[playerIndex].name); //msg is who won
+			}
+			else if (round){
+				dealDeck();
+				lastRoundWinner = players[playerIndex].name; //should be a string for player name
+				io.emit("roundWin", lastRoundWinner);
+			}
+
 		}
 		else {
-			//no win, just keep playing
-			io.emit("noWin", msg);
+			io.emit("noWin"); //don't need a msg, game play continues
 		}
 		updateGameState();
 	});
 
 });
 
-mongoClient.connect("mongodb://localhost:27017", function(err, database) {
+mongoClient.connect("mongodb://localhost:27017/pit", function(err, database) {
 	if (err) throw err;
 	db = database;
 	collection = db.collection("users");
